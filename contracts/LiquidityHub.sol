2e0ebd75b31a9d49f1f5d69a32eaddec48dc07a5
Oleksii Matiiasevych <oleksii@chainsafe.io>
2025-01-21 21:52:58 +0700

    WIP: add adjustment and 2 step process

--- contracts/LiquidityHub.sol
+++ contracts/LiquidityHub.sol
@@ -6,6 +6,7 @@ import {
     IERC20Metadata,
     ERC20Upgradeable,
     ERC4626Upgradeable,
+    SafeERC20,
     Math
 } from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
 import {AccessControlUpgradeable} from '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
@@ -18,13 +19,39 @@ contract LiquidityHub is ERC4626Upgradeable, AccessControlUpgradeable {
     IManagedToken immutable public SHARES;
     bytes32 public constant ASSETS_UPDATE_ROLE = "ASSETS_UPDATE_ROLE";
 
+    event TotalAssetsAdjustment(uint256 oldAssets, uint256 newAssets);
+    event DepositRequest(address caller, address receiver, uint256 assets);
+    event WithdrawRequest(address caller, address receiver, address owner, uint256 shares);
+
     error ZeroAddress();
     error NotImplemented();
     error IncompatibleAssetsAndShares();
 
+    struct AdjustmentRecord {
+        uint256 totalAssets;
+        uint256 totalShares;
+    }
+
+    struct PendingDeposit {
+        uint256 assets;
+        uint256 adjustmentId;
+    }
+
+    struct PendingWithdraw {
+        uint256 shares;
+        uint256 adjustmentId;
+    }
+
     /// @custom:storage-location erc7201:sprinter.storage.LiquidityHub
     struct LiquidityHubStorage {
         uint256 totalAssets;
+        uint256 totalShares;
+        uint256 depositedAssets;
+        uint256 burnedShares;
+        uint256 lastAdjustmentId;
+        mapping(uint256 adjustmentId => AdjustmentRecord) adjustmentRecords;
+        mapping(address receiver => PendingDeposit) pendingDeposits;
+        mapping(address receiver => PendingWithdraw) pendingWithdrawals;
     }
 
     bytes32 private constant StorageLocation = 0xb877bfaae1674461dd1960c90f24075e3de3265a91f6906fe128ab8da6ba1700;
@@ -50,6 +77,24 @@ contract LiquidityHub is ERC4626Upgradeable, AccessControlUpgradeable {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
     }
 
+    function adjustTotalAssets(uint256 amount, bool isIncrease) external onlyRole(ASSETS_UPDATE_ROLE) {
+        LiquidityHubStorage storage $ = _getStorage();
+        uint256 adjustmentId = ++$.lastAdjustmentId;
+        AdjustmentRecord storage adjustmentRecord = $.adjustmentRecords[adjustmentId];
+        uint256 assets = $.totalAssets;
+        uint256 newAssets = isIncrease ? assets + amount : assets - amount;
+        uint256 supplyShares = totalSupply();
+        uint256 mintingShares = _toShares($.depositedAssets, supplyShares, newAssets, Math.Rounding.Floor);
+        uint256 releasingAssets = _toAssets($.burnedShares, supplyShares, newAssets, Math.Rounding.Floor);
+        $.totalAssets = newAssets - releasingAssets;
+        $.totalShares = supplyShares + mintingShares - $.burnedShares;
+        $.depositedAssets = 0;
+        $.burnedShares = 0;
+        adjustmentRecord.totalAssets = newAssets;
+        adjustmentRecord.totalShares = supplyShares;
+        emit TotalAssetsAdjustment(assets, newAssets);
+    }
+
     function name() public pure override(IERC20Metadata, ERC20Upgradeable) returns (string memory) {
         revert NotImplemented();
     }
@@ -63,7 +108,7 @@ contract LiquidityHub is ERC4626Upgradeable, AccessControlUpgradeable {
     }
 
     function totalSupply() public view virtual override(IERC20, ERC20Upgradeable) returns (uint256) {
-        return IERC20(address(SHARES)).totalSupply();
+        return _getStorage().totalShares;
     }
 
     function balanceOf(address owner) public view virtual override(IERC20, ERC20Upgradeable) returns (uint256) {
@@ -91,26 +136,42 @@ contract LiquidityHub is ERC4626Upgradeable, AccessControlUpgradeable {
         return _getStorage().totalAssets;
     }
 
-    function _convertToShares(uint256 assets, Math.Rounding rounding) internal view virtual override returns (uint256) {
-        (uint256 supplyShares, uint256 supplyAssets) = _getTotals();
+    function _toShares(
+        uint256 assets,
+        uint256 supplyShares,
+        uint256 supplyAssets,
+        Math.Rounding rounding
+    ) internal view returns (uint256) {
+        (supplyShares, supplyAssets) = _getTotals(supplyShares, supplyAssets);
         return assets.mulDiv(supplyShares, supplyAssets, rounding);
     }
 
-    function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view virtual override returns (uint256) {
-        (uint256 supplyShares, uint256 supplyAssets) = _getTotals();
+    function _toAssets(
+        uint256 shares,
+        uint256 supplyShares,
+        uint256 supplyAssets,
+        Math.Rounding rounding
+    ) internal view returns (uint256) {
+        (supplyShares, supplyAssets) = _getTotals(supplyShares, supplyAssets);
         return shares.mulDiv(supplyAssets, supplyShares, rounding);
     }
 
-    function _getTotals() internal view returns (uint256 supply, uint256 assets) {
-        supply = totalSupply();
-        if (supply == 0) {
-            supply = 10 ** _decimalsOffset();
+    function _convertToShares(uint256 assets, Math.Rounding rounding) internal view virtual override returns (uint256) {
+        return _toShares(assets, totalSupply(), totalAssets(), rounding);
+    }
+
+    function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view virtual override returns (uint256) {
+        return _toAssets(shares, totalSupply(), totalAssets(), rounding);
+    }
+
+    function _getTotals(uint256 supplyShares, uint256 supplyAssets) internal view returns (uint256, uint256) {
+        if (supplyShares == 0) {
+            supplyShares = 10 ** _decimalsOffset();
         }
-        assets = totalAssets();
-        if (assets == 0) {
-            assets = 1;
+        if (supplyAssets == 0) {
+            supplyAssets = 1;
         }
-        return (supply, assets);
+        return (supplyShares, supplyAssets);
     }
 
     function _update(address from, address to, uint256 value) internal virtual override {
@@ -127,20 +188,76 @@ contract LiquidityHub is ERC4626Upgradeable, AccessControlUpgradeable {
         SHARES.spendAllowance(owner, spender, value);
     }
 
-    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual override {
-        super._deposit(caller, receiver, assets, shares);
-        _getStorage().totalAssets += assets;
+    function _deposit(address caller, address receiver, uint256 assets, uint256 /*shares*/) internal virtual override {
+        LiquidityHubStorage storage $ = _getStorage();
+        SafeERC20.safeTransferFrom(IERC20(asset()), caller, address(this), assets);
+        _settleDeposit(caller, receiver);
+        PendingDeposit storage pendingDeposit = $.pendingDeposits[receiver];
+        pendingDeposit.assets += assets;
+        pendingDeposit.adjustmentId = $.lastAdjustmentId;
+        emit DepositRequest(caller, receiver, assets);
+    }
+
+    function _settleDeposit(address caller, address receiver) internal {
+        LiquidityHubStorage storage $ = _getStorage();
+        PendingDeposit storage pendingDeposit = $.pendingDeposits[receiver];
+        uint256 assets = pendingDeposit.assets;
+        uint256 settleAdjustmentId = pendingDeposit.adjustmentId + 1;
+        if (assets == 0) {
+            return;
+        }
+        if (settleAdjustmentId > $.lastAdjustmentId) {
+            return;
+        }
+        pendingDeposit.assets = 0;
+        pendingDeposit.adjustmentId = 0;
+        AdjustmentRecord memory adjustmentRecord = $.adjustmentRecords[settleAdjustmentId];
+        uint256 shares = _toShares(
+            assets, adjustmentRecord.totalShares, adjustmentRecord.totalAssets, Math.Rounding.Floor
+        );
+        _mint(receiver, shares);
+        emit Deposit(caller, receiver, assets, shares);
     }
 
     function _withdraw(
         address caller,
         address receiver,
         address owner,
-        uint256 assets,
+        uint256 /*assets*/,
         uint256 shares
     ) internal virtual override {
-        _getStorage().totalAssets -= assets;
-        super._withdraw(caller, receiver, owner, assets, shares);
+        LiquidityHubStorage storage $ = _getStorage();
+        if (caller != owner) {
+            _spendAllowance(owner, caller, shares);
+        }
+        _settleWithdraw(caller, receiver, owner);
+        PendingWithdraw storage pendingWithdraw = $.pendingWithdrawals[receiver];
+        pendingWithdraw.shares += shares;
+        pendingWithdraw.adjustmentId = $.lastAdjustmentId;
+        $.burnedShares += shares;
+        _burn(owner, shares);
+        emit WithdrawRequest(caller, receiver, owner, shares);
+    }
+
+    function _settleWithdraw(address caller, address receiver, address owner) internal {
+        LiquidityHubStorage storage $ = _getStorage();
+        PendingWithdraw storage pendingWithdraw = $.pendingWithdrawals[receiver];
+        uint256 shares = pendingWithdraw.shares;
+        uint256 settleAdjustmentId = pendingWithdraw.adjustmentId + 1;
+        if (shares == 0) {
+            return;
+        }
+        if (settleAdjustmentId > $.lastAdjustmentId) {
+            return;
+        }
+        pendingWithdraw.shares = 0;
+        pendingWithdraw.adjustmentId = 0;
+        AdjustmentRecord memory adjustmentRecord = $.adjustmentRecords[settleAdjustmentId];
+        uint256 assets = _toAssets(
+            shares, adjustmentRecord.totalShares, adjustmentRecord.totalAssets, Math.Rounding.Floor
+        );
+        SafeERC20.safeTransfer(IERC20(asset()), receiver, assets);
+        emit Withdraw(caller, receiver, owner, assets, shares);
     }
 
     function _decimalsOffset() internal view virtual override returns (uint8) {

c3931f5995ff65ae020ed57a17103c775ef22703
Oleksii Matiiasevych <oleksii@sprinter.tech>
2025-11-13 18:48:51 +0700

    Update Everclear adapter, tests, add config

--- contracts/utils/EverclearAdapter.sol
+++ contracts/utils/EverclearAdapter.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity 0.8.28;
 
-import {IFeeAdapter} from ".././interfaces/IEverclear.sol";
+import {IFeeAdapterV2} from ".././interfaces/IEverclear.sol";
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {AdapterHelper} from "./AdapterHelper.sol";
 
 abstract contract EverclearAdapter is AdapterHelper {
     using SafeERC20 for IERC20;
 
-    IFeeAdapter immutable public EVERCLEAR_FEE_ADAPTER;
+    IFeeAdapterV2 immutable public EVERCLEAR_FEE_ADAPTER;
 
     constructor(
         address everclearFeeAdapter
     ) {
         // No check for address(0) to allow deployment on chains where Everclear is not available
-        EVERCLEAR_FEE_ADAPTER = IFeeAdapter(everclearFeeAdapter);
+        EVERCLEAR_FEE_ADAPTER = IFeeAdapterV2(everclearFeeAdapter);
     }
 
     function initiateTransferEverclear(
@@ -28,12 +28,11 @@ abstract contract EverclearAdapter is AdapterHelper {
         token.forceApprove(address(EVERCLEAR_FEE_ADAPTER), amount);
         (
             bytes32 outputAsset,
-            uint256 outputAmount,
-            uint24 maxFee,
+            uint256 amountOutMin,
             uint48 ttl,
-            IFeeAdapter.FeeParams memory feeParams
-        ) = abi.decode(extraData, (bytes32, uint256, uint24, uint48, IFeeAdapter.FeeParams));
-        require(outputAmount >= (amount * 9980 / 10000), SlippageTooHigh());
+            IFeeAdapterV2.FeeParams memory feeParams
+        ) = abi.decode(extraData, (bytes32, uint256, uint48, IFeeAdapterV2.FeeParams));
+        require(amountOutMin >= (amount * 9980 / 10000), SlippageTooHigh());
         uint32[] memory destinations = new uint32[](1);
         destinations[0] = domainChainId(destinationDomain);
         EVERCLEAR_FEE_ADAPTER.newIntent{value: msg.value}(
@@ -41,8 +40,8 @@ abstract contract EverclearAdapter is AdapterHelper {
             _addressToBytes32(destinationPool),
             address(token),
             outputAsset,
-            outputAmount,
-            maxFee,
+            amount - feeParams.fee,
+            amountOutMin,
             ttl,
             "",
             feeParams

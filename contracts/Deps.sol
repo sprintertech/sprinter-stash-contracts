aedcebe9f2ba16198c68466272238d5bdb34856f
Oleksii Matiiasevych <oleksii@sprinter.tech>
2025-12-18 13:39:47 +0700

    Trying other things

--- contracts/Deps.sol
+++ contracts/Deps.sol
@@ -3,4 +3,5 @@ pragma solidity 0.8.28;
 
 /* solhint-disable no-unused-import */
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+/* solhint-disable no-unused-import */
 import {console} from "hardhat/console.sol";
